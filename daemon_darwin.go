aa2106647f2f56b2cea8705467e62e24345b7f40
Stephen Fox <stephen.j.fox.jr@gmail.com>
2019-08-01 23:32:37 -0400

    Check 'PS1' env. after bool on macOS.

--- daemon_darwin.go
+++ daemon_darwin.go
@@ -61,7 +61,7 @@ func (o *darwinDaemon) RunUntilExit(logic ApplicationLogic) error {
 	// The 'PS1' environment variable will be empty / not set when
 	// this is run non-interactively. Only do native log things
 	// when running non-interactively.
-	if len(os.Getenv("PS1")) == 0 && o.logConfig.UseNativeLogger {
+	if o.logConfig.UseNativeLogger && len(os.Getenv("PS1")) == 0 {
 		err := os.MkdirAll(path.Dir(o.stderrLogFilePath), 0700)
 		if err != nil {
 			return err
