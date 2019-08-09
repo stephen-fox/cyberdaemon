c97210691f7a78f4cf728d4e1fdf77914adaa447
Stephen Fox <stephen.j.fox.jr@gmail.com>
2019-08-09 19:35:21 -0400

    Added some needed TODOs to the 'Config' struct.

--- common.go
+++ common.go
@@ -53,6 +53,13 @@ type ApplicationLogic interface {
 	Stop() error
 }
 
+// TODO: Additional daemon configuration:
+//  - Manually setting daemon executable file path
+//  - Enabling the daemon when it is installed, or started
+//    (e.g., 'systemctl enable <whatever>', or Windows service's
+//    autostart feature)
+//  - Rename 'Username' field to 'RunAs'
+//  - Support OS specific options
 type Config struct {
 	DaemonId    string
 	Description string
