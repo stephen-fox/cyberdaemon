3ed541f3cf532747223645ed9152beed093a2fbd
Stephen Fox <stephen.j.fox.jr@gmail.com>
2019-08-17 11:16:58 -0400

    Moved Windows password option into dedicated options file.

--- daemon_windows.go
+++ daemon_windows.go
@@ -15,34 +15,8 @@ import (
 )
 
 const (
-	// PasswordOption is used to specify the password for a user when
-	// installing a daemon that will run as that user. Here is an example
-	// ControllerConfig that reads the user's password from an
-	// environment variable:
-	//
-	//	config := cyberdaemon.ControllerConfig{
-	//		DaemonID:              "test",
-	//		Description:           "I need my guys. They're the best.",
-	//		RunAs:                 ".\\stephen",
-	//		SystemSpecificOptions: map[cyberdaemon.SystemSpecificOption]interface{}{
-	//			cyberdaemon.PasswordOption: cyberdaemon.GetPassword(func() (string, error) {
-	//				p, ok := os.LookupEnv("WHYARETHEYTHEBEST")
-	//				if !ok {
-	//					return "", fmt.Errorf("password environment variable was not set")
-	//				}
-	//				return p, nil
-	//			}),
-	//		},
-	//	}
-	PasswordOption SystemSpecificOption = "password"
-
 	notInstalledErr = "The specified service does not exist as an installed service."
 )
-
-// GetPassword represents a function that will return a user's password when
-// installing a daemon that will run as that user. See the documentation for
-// PasswordOption for more information.
-type GetPassword func() (string, error)
 
 type windowsController struct {
 	config       ControllerConfig
